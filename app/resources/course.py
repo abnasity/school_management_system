01681ba4d50db87e0bceb6e4e17ab66c8d0102aa
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-24 20:40:10 +0300

    parse course requests

--- app/resources/course.py
+++ app/resources/course.py
@@ -4,3 +4,9 @@ from app.models.course import CourseModel
 from dateutil import parser as date_parser
 
 
+# REQUEST PARSER
+course_args = reqparse.RequestParser()
+course_args.add_argument('code', type=int, required=True, help="Course code cannot be empty")
+course_args.add_argument('name', type=str, required=True, help="Course name cannot be empty")
+course_args.add_argument('credits', type=int, default=0, help="Credits must be an integer")
+course_args.add_argument('teacher_id', type=int, required=True, help="Teacher ID is required")
