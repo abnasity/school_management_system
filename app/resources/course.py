62b1c20882b95ad908ab68501bf6211bfca34290
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-24 20:50:16 +0300

    course fields

--- app/resources/course.py
+++ app/resources/course.py
@@ -10,3 +10,13 @@ course_args.add_argument('code', type=int, required=True, help="Course code cann
 course_args.add_argument('name', type=str, required=True, help="Course name cannot be empty")
 course_args.add_argument('credits', type=int, default=0, help="Credits must be an integer")
 course_args.add_argument('teacher_id', type=int, required=True, help="Teacher ID is required")
+
+
+# COURSE FIELDS
+course_fields = {
+    'id' : fields.Integer,
+    'code': fields.String,
+    'name': fields.String,
+    'credits': fields.Integer,
+    'teacher_id': fields.Integer
+}
