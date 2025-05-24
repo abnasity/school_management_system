769ec46cc216afac12f5e82f52d550bb83feb548
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-24 20:27:00 +0300

    import modules

--- app/resources/course.py
+++ app/resources/course.py
@@ -1,1 +1,6 @@
 from flask_restful import Resource, abort, marshal_with, fields, reqparse
+from app.extension import db
+from app.models.course import CourseModel
+from dateutil import parser as date_parser
+
+
