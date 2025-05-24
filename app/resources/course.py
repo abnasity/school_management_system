080d8cd5f50673d4b0e965ff49b4f8ec23dcfaae
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-25 00:23:53 +0300

    removed undesired import

--- app/resources/course.py
+++ app/resources/course.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, abort, marshal_with, fields, reqparse
 from app.extension import db
 from app.models.course import CourseModel
-from dateutil import parser as date_parser
+
 
 
 # REQUEST PARSER
