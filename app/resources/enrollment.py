4eaa1fd34e71e8e592ad92d87be90d00ed020e9b
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-24 17:47:06 +0300

    enrollment args cmplete

--- app/resources/enrollment.py
+++ app/resources/enrollment.py
@@ -6,3 +6,7 @@ from dateutil import parser as date_parser
 
 #Request Parser
 enrollment_args = reqparse.RequestParser()
+enrollment_args.add_argument('student_id', type=int, help="Student ID cannot be empty")
+enrollment_args.add_argument('course_id', type=str, help="Course ID cannot be empty")
+enrollment_args.add_argument('enrollment_date', type=date_parser)
+enrollment_args.add_argument('status', type=str, default='active') #if the client does not provide a status value it'll be set to active by default
