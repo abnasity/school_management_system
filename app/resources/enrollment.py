ddc422a2c5ad6964ea5c2ed2d781dbd887feee8c
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-24 19:10:34 +0300

    patch args

--- app/resources/enrollment.py
+++ app/resources/enrollment.py
@@ -24,10 +24,10 @@ enrollment_fields = {
 # Enrollments Resource
 class Enrollment(Resource):
     @marshal_with(enrollment_fields)
-    def get(self):
+    def get(self, id):
         # Implement the logic for GET request here
         # For example, return all enrollments
-        enrollments = EnrollmentModel.query.all()
+        enrollments = EnrollmentModel.query.filter_by(id=id).first
         if not enrollments:
             abort (404, message="Enrollments not found")
         return enrollments
@@ -35,12 +35,36 @@ class Enrollment(Resource):
     @marshal_with(enrollment_fields)
     def post(self):
         args = enrollment_args.parse_args()
-        enrollment = EnrollmentModel(
-            student_id=args['student_id'],
-            course_id=args['course_id'],
-            enrollment_date=args['enrollment_date'],
-            status=args['status']
-        )
-        db.session.add(enrollment)
-        db.session.commit()
-        return enrollment, 201
+        try:
+            enrollment = EnrollmentModel(
+                student_id=args['student_id'],
+                course_id=args['course_id'],
+                enrollment_date=args['enrollment_date'],
+                status=args['status']
+            )
+            db.session.add(enrollment)
+            db.session.commit()
+            return enrollment, 201
+        except Exception as e:
+            db.session.rollback()
+            abort(400, message=f"Error: Could not create an enrollment. {str(e)}")
+            
+    @marshal_with(enrollment_fields)
+    def patch(self, id):
+        args = enrollment_args.parse_args()
+        enrollment = EnrollmentModel.query.filter_by(id=id).first()
+        if not enrollment:
+            abort(404, message="Enrollment not found")
+        try:
+            enrollment.student_id = args['student_id']
+       
+            enrollment.course_id = args['course_id']
+
+            enrollment.enrollment_date = args['enrollment_date']
+       
+            enrollment.status = args['status']
+            db.session.commit()
+            return enrollment, 200
+        except Exception as e:
+             db.session.rollback()
+             abort(400, message=f"could not update the enrollment. {str(e)}")
