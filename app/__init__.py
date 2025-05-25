2f8f88a7bbe50940aefcb5811301047912444a4f
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-25 19:06:09 +0300

    add api resources

--- app/__init__.py
+++ app/__init__.py
@@ -17,10 +17,19 @@ migrate = Migrate(app,db)
  #api endpoints
 api.add_resource(Users,'/api/users/')
 api.add_resource(User,'/api/users/<int:id>')
+
 api.add_resource(Teachers, '/api/teachers')
 api.add_resource(Teacher, '/api/teachers/<int:id>')
 
-    
+api.add_resource(Students, '/api/students')
+api.add_resource(Student, '/api/students/<int:id>')
 
 
+api.add_resource(Courses, '/api/courses')
+api.add_resource(course, '/api/courses/<int:id>')
 
+api.add_resource(Enrollments, '/api/enrollments')
+api.add_resource(Enrollment, '/api/enrollments/<int:id>')
+
+api.add_resource(Fees, '/api/fees')
+api.add_resource(Fee, '/api/fees/<int:id>')
