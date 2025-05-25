76d59cd3fe392ff88a023f6672441108222ff8cd
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-25 17:52:28 +0300

    request parse

--- app/resources/student.py
+++ app/resources/student.py
@@ -1,6 +1,13 @@
 from flask_restful import Resource, abort, marshal_with, fields, reqparse
 from app.extension import db
 from app.models.student import StudentModel
-
+from dateutil.parser import parser as date_parse
 
 # Request Parser
+student_args = reqparse.RequestParser()
+student_args.add_argument('first_name',type=str, required=True, help="First name cannot be empty")
+student_args.add_argument('last_name', type=str, required=True, help="Last name cannot be empty")
+student_args.add_argument('student_id', type=str, required=True, help="Student ID cannot be blank")
+student_args.add_argument('email', type=str, required=True, help="student email cannot be blank")
+student_args.add_argument('date_of_birth', type=date_parse)
+student_args.add_argument('enrollment_date', type=date_parse)
