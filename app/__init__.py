ece5994beb83d68ce3fd9ec20ebf81f84e93a816
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-27 15:40:21 +0300

    configured swagger ui to flasker

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from config import Config
+from flask_migrate import Migrate
+from flasgger import Swagger
 from app.extension import db
 from flask_restful import Api
 from app.resources.user import Users,User
@@ -7,8 +10,75 @@ from app.resources.student import Students,Student
 from app.resources.enrollment import Enrollments, Enrollment
 from app.resources.fee import Fees,Fee
 from app.resources.course import Courses, Course
-from config import Config
-from flask_migrate import Migrate
+
+# swagger configuration
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
+
+# API Information Template
+template = {
+    "info": {
+        "title": "School Management System API",
+        "description": "API for managing school operations including students, teachers, courses, enrollments, and fees.",
+        "version": "1.0.0",
+        "contact": {
+            "name": "API Support",
+            "url": "http://example.com/support",
+            "email": "abdkpng@gmail.com"
+            }
+        },
+        "host": "localhost:5000",
+        "basePath": "/api",
+        "schemes": ["http", "https"],
+        "tags": [
+            {
+                "name": "Users",
+                "description": "Operations related to users"
+            },
+            {
+                "name": "Teachers",
+                "description": "Operations related to teachers"
+            },
+            {
+                "name": "Students",
+                "description": "Operations related to students"
+            },
+            {
+                "name": "Courses",
+                "description": "Operations related to courses"
+            },
+            {
+                "name": "Enrollments",
+                "description": "Operations related to enrollments"
+            },
+            {
+                "name": "Fees",
+                "description": "Operations related to fees"
+            }
+        ]
+     
+    
+            
+        
+}
+# 
+
+
+    
 
 
 
@@ -17,6 +87,7 @@ app.config.from_object(Config)
 db.init_app(app)
 api = Api(app)
 migrate = Migrate(app,db)
+swagger = Swagger(app, config=swagger_config, template=template)
 
  #api endpoints
 api.add_resource(Users,'/api/users/')
